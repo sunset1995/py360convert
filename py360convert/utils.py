22f367b77682c0d19cb6782bb8bc667a0f9fb6ea
Brian Pugh <bnp117@gmail.com>
2024-12-15 10:26:15 -0500

    migrate to poetry; initial linting

--- py360convert/utils.py
+++ py360convert/utils.py
@@ -3,36 +3,36 @@ from scipy.ndimage import map_coordinates
 
 
 def xyzcube(face_w):
-    '''
-    Return the xyz cordinates of the unit cube in [F R B L U D] format.
-    '''
+    """
+    Return the xyz coordinates of the unit cube in [F R B L U D] format.
+    """
     out = np.zeros((face_w, face_w * 6, 3), np.float32)
     rng = np.linspace(-0.5, 0.5, num=face_w, dtype=np.float32)
     grid = np.stack(np.meshgrid(rng, -rng), -1)
 
     # Front face (z = 0.5)
-    out[:, 0*face_w:1*face_w, [0, 1]] = grid
-    out[:, 0*face_w:1*face_w, 2] = 0.5
+    out[:, 0 * face_w : 1 * face_w, [0, 1]] = grid
+    out[:, 0 * face_w : 1 * face_w, 2] = 0.5
 
     # Right face (x = 0.5)
-    out[:, 1*face_w:2*face_w, [2, 1]] = grid
-    out[:, 1*face_w:2*face_w, 0] = 0.5
+    out[:, 1 * face_w : 2 * face_w, [2, 1]] = grid
+    out[:, 1 * face_w : 2 * face_w, 0] = 0.5
 
     # Back face (z = -0.5)
-    out[:, 2*face_w:3*face_w, [0, 1]] = grid
-    out[:, 2*face_w:3*face_w, 2] = -0.5
+    out[:, 2 * face_w : 3 * face_w, [0, 1]] = grid
+    out[:, 2 * face_w : 3 * face_w, 2] = -0.5
 
     # Left face (x = -0.5)
-    out[:, 3*face_w:4*face_w, [2, 1]] = grid
-    out[:, 3*face_w:4*face_w, 0] = -0.5
+    out[:, 3 * face_w : 4 * face_w, [2, 1]] = grid
+    out[:, 3 * face_w : 4 * face_w, 0] = -0.5
 
     # Up face (y = 0.5)
-    out[:, 4*face_w:5*face_w, [0, 2]] = grid
-    out[:, 4*face_w:5*face_w, 1] = 0.5
+    out[:, 4 * face_w : 5 * face_w, [0, 2]] = grid
+    out[:, 4 * face_w : 5 * face_w, 1] = 0.5
 
     # Down face (y = -0.5)
-    out[:, 5*face_w:6*face_w, [0, 2]] = grid
-    out[:, 5*face_w:6*face_w, 1] = -0.5
+    out[:, 5 * face_w : 6 * face_w, [0, 2]] = grid
+    out[:, 5 * face_w : 6 * face_w, 1] = -0.5
 
     return out
 
@@ -45,9 +45,9 @@ def equirect_uvgrid(h, w):
 
 
 def equirect_facetype(h, w):
-    '''
+    """
     0F 1R 2B 3L 4U 5D
-    '''
+    """
     tp = np.roll(np.arange(4).repeat(w // 4)[None, :].repeat(h, 0), 3 * w // 8, 1)
 
     # Prepare ceil mask
@@ -80,9 +80,9 @@ def xyzpers(h_fov, v_fov, u, v, out_hw, in_rot):
 
 
 def xyz2uv(xyz):
-    '''
+    """
     xyz: ndarray in shape of [..., 3]
-    '''
+    """
     x, y, z = np.split(xyz, 3, axis=-1)
     u = np.arctan2(x, z)
     c = np.sqrt(x**2 + z**2)
@@ -102,11 +102,11 @@ def uv2unitxyz(uv):
 
 
 def uv2coor(uv, h, w):
-    '''
+    """
     uv: ndarray in shape of [..., 2]
     h: int, height of the equirectangular image
     w: int, width of the equirectangular image
-    '''
+    """
     u, v = np.split(uv, 2, axis=-1)
     coor_x = (u / (2 * np.pi) + 0.5) * w - 0.5
     coor_y = (-v / np.pi + 0.5) * h - 0.5
@@ -128,8 +128,7 @@ def sample_equirec(e_img, coor_xy, order):
     pad_u = np.roll(e_img[[0]], w // 2, 1)
     pad_d = np.roll(e_img[[-1]], w // 2, 1)
     e_img = np.concatenate([e_img, pad_d, pad_u], 0)
-    return map_coordinates(e_img, [coor_y, coor_x],
-                           order=order, mode='wrap')[..., 0]
+    return map_coordinates(e_img, [coor_y, coor_x], order=order, mode="wrap")[..., 0]
 
 
 def sample_cubefaces(cube_faces, tp, coor_y, coor_x, order):
@@ -170,7 +169,7 @@ def sample_cubefaces(cube_faces, tp, coor_y, coor_x, order):
     pad_lr[5, 1:-1, 1] = cube_faces[3, -2, ::-1]
     cube_faces = np.concatenate([cube_faces, pad_lr], 2)
 
-    return map_coordinates(cube_faces, [tp, coor_y, coor_x], order=order, mode='wrap')
+    return map_coordinates(cube_faces, [tp, coor_y, coor_x], order=order, mode="wrap")
 
 
 def cube_h2list(cube_h):
@@ -186,11 +185,10 @@ def cube_list2h(cube_list):
 
 def cube_h2dict(cube_h):
     cube_list = cube_h2list(cube_h)
-    return dict([(k, cube_list[i])
-                 for i, k in enumerate(['F', 'R', 'B', 'L', 'U', 'D'])])
+    return dict([(k, cube_list[i]) for i, k in enumerate(["F", "R", "B", "L", "U", "D"])])
 
 
-def cube_dict2h(cube_dict, face_k=['F', 'R', 'B', 'L', 'U', 'D']):
+def cube_dict2h(cube_dict, face_k=["F", "R", "B", "L", "U", "D"]):
     assert len(face_k) == 6
     return cube_list2h([cube_dict[k] for k in face_k])
 
@@ -208,7 +206,7 @@ def cube_h2dice(cube_h):
             face = np.flip(face, axis=1)
         if i == 4:
             face = np.flip(face, axis=0)
-        cube_dice[sy*w:(sy+1)*w, sx*w:(sx+1)*w] = face
+        cube_dice[sy * w : (sy + 1) * w, sx * w : (sx + 1) * w] = face
     return cube_dice
 
 
@@ -219,12 +217,12 @@ def cube_dice2h(cube_dice):
     # Order: F R B L U D
     sxy = [(1, 1), (2, 1), (3, 1), (0, 1), (1, 0), (1, 2)]
     for i, (sx, sy) in enumerate(sxy):
-        face = cube_dice[sy*w:(sy+1)*w, sx*w:(sx+1)*w]
+        face = cube_dice[sy * w : (sy + 1) * w, sx * w : (sx + 1) * w]
         if i in [1, 2]:
             face = np.flip(face, axis=1)
         if i == 4:
             face = np.flip(face, axis=0)
-        cube_h[:, i*w:(i+1)*w] = face
+        cube_h[:, i * w : (i + 1) * w] = face
     return cube_h
 
 
@@ -236,8 +234,6 @@ def rotation_matrix(rad, ax):
     R = R + np.outer(ax, ax) * (1.0 - np.cos(rad))
 
     ax = ax * np.sin(rad)
-    R = R + np.array([[0, -ax[2], ax[1]],
-                      [ax[2], 0, -ax[0]],
-                      [-ax[1], ax[0], 0]])
+    R = R + np.array([[0, -ax[2], ax[1]], [ax[2], 0, -ax[0]], [-ax[1], ax[0], 0]])
 
     return R
---
+++ tests/test_e2c.py
@@ -0,0 +1,2 @@
+def test_c2e():
+    """TODO."""
